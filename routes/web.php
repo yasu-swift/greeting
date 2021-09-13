21281371d9cfb649c8f3284f9bba8338dd60c324
yasu-swift <sp4c72q9@icloud.com>
2021-09-14 01:27:00 +0900

    あいさつアプリ課題完了

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'hello!';
+});
+
+Route::get('/comments/{a}', function ($a) {
+    if ($a == 'morning') {
+        echo "<h1>朝のあいさつ</h1>". '<br>'. "<h2>おはようございます</h2>" ;
+    }elseif ($a == 'afternoon') {
+        echo "<h1>昼のあいさつ</h1>". '<br>'. "<h2>こんにちは</h2>" ;
+    }elseif ($a == 'evening') {
+        echo "<h1>夕方のあいさつ</h1>". '<br>'. "<h2>こんばんは</h2>" ;
+    }elseif ($a == 'night') {
+        echo "<h1>夜のあいさつ</h1>". '<br>'. "<h2>おやすみ</h2>" ;
+    }elseif ($a == 'ramdon'){
+        srand((float) microtime() * 10000000);
+$input = array("<h2>おはよう</h2>", "<h2>こんにちは</h2>", "<h2>こんばんは</h2>", "<h2>おやすみなさい</h2>");
+$rand_keys = array_rand($input, 2);
+echo "<h1>ランダムなメッセージ</h1>". "<br>". $input[$rand_keys[0]] . "\n";
+    }
+    else{
+        echo "<h1>スペルが間違ってませんか？</h2>";
+    }
+});
+
+Route::get('/comments/{a}/{b}', function($a, $b){
+    if ($a == 'freeword') {
+        echo "<h1>自由なメッセージ</h1>". '<br>'.
+            "<h2>$b</h2>";
+    }
 });
